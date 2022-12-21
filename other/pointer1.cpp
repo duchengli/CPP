0822c69a26d667031bb854275b4dd6d5c292485a
duchengli <duchengli@hotmail.com>
2022-12-21 16:53:37 +0800

    20221221@homev2

--- other/pointer1.cpp
+++ other/pointer1.cpp
@@ -5,39 +5,20 @@ using namespace std;
 
 int main()
 {
-    // 对象和地址
-    int n;
-    double x;
-    cout << "n的地址：" << &n << endl;
-    cout << "x的地址：" << &x << endl;
-    // 对象占用的内存空间大小
-    cout << "n的内存空间大小：" << sizeof(n) << endl;
-    cout << "x的内存空间大小：" << sizeof(x) << endl;
-    // 对象的类型
-    cout << "n的类型名：" << typeid(n).name() << endl;
-    cout << "x的类型名：" << typeid(x).name() << endl;
-    // 指针的基础
-    int num = 35;
-    int *p = &num;
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *p = arr;
+    cout << "p中存放的地址：" << p << endl;
+    cout << "p中地址的值：" << *p << endl;
 
-    cout << "num:" << num << endl;
-    cout << "地址 &num:" << &num << endl;
-    cout << "解引用 *p:" << *p << endl;
-    cout << "取地址 &p:" << &p << endl;
-    cout << "p:" << p << endl;
+    ++p;
+    cout << "++p中存放的地址：" << p << endl;
+    cout << "++p中地址的值：" << *p << endl;
 
-    // 指针的类型
-    int *p1, *p2;
-    double dp, *dp2;
-
-    cout << "p1的类型名：" << typeid(p1).name() << endl;
-    cout << "p2的类型名：" << typeid(p2).name() << endl;
-    cout << "dp的类型名：" << typeid(dp).name() << endl;
-    cout << "dp2的类型名：" << typeid(dp2).name() << endl;
-    // 指针的大小
-    cout << "p1的内存空间大小：" << sizeof(p1) << endl;
-    cout << "dp2的内存空间大小：" << sizeof(dp2) << endl;
-    cout << "int* 的大小：" << sizeof(int *) << endl;
+    int *e = &arr[9];
+    for (int *b = arr; b <= e; b++)
+    {
+        cout << *b << endl;
+    }
 
     return 0;
 }
